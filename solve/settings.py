11df8fecb1871ca0df905f311d6ed19029823bab
mariusgronli <mariusgronli@gmail.com>
2020-10-28 11:26:12 +0000

    changed allowed hosts

--- solve/settings.py
+++ solve/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'mariusgronli.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mariusdev.pythonanywhere.com']
 
 
 # Application definition
