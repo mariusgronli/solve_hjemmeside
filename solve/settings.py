12913313f303e455abc9e195d62bf19f4c577da4
mariusgronli <mariusgronli@gmail.com>
2020-12-02 16:21:52 +0000

    changed settings

--- solve/settings.py
+++ solve/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'mariusdev.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mariusdev.pythonanywhere.com','solvesundre.pythonanywhere.com']
 
 
 # Application definition
